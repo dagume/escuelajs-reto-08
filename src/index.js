e018959d1788c805b4cdfb14b8247871a08eadc2
Daniel Guecha <guechadaniel@gmail.com>
2020-09-29 19:32:01 -0500

    :sparkles: challenge done

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 const express = require("express"),
   path = require("path"),
   app = express(),
-  port = process.env.PORT || 3000;
-
+  { config } = require("./config/index"),
+  { platziStore } = require("./utils/mocks/platziStore"),
+  port = config.port || 3000;
+  
 app.get('/', (req, res) => {
   let userInfo = req.header("user-agent");
   res.send(`UserInfo: ${userInfo}`);
 });
 
 app.get('/receipts', (req, res) => {
-  let file = path.join(__dirname, "asset/receipt.pdf");
-  res.sendFile();
+  let file = path.join(__dirname, "assets/receipt.pdf");
+  res.sendFile(file);
 });
 
 app.get('/products', (req, res) => {
-  let storeProducts = '';
+  let storeProducts = platziStore;
   res.json(storeProducts);
 });
 
